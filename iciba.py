cbd4cc20d09846e70a4ae2116ebfd06255709667
poc7667 <poc7667@gmail.com>
2013-07-15 14:39:39 +0800

    Refine the layout

--- iciba.py
+++ iciba.py
@@ -8,8 +8,11 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 _REQ_HTTP_PREFIX="http://www.iciba.com/"
-
 _REQ_TIMEOUTS=10
+_DEBUG=True
+_result_file=''
+_RESULT_FILE_NAME='./result_file.txt'
+
 
 def found_result(soup):
     res = soup.find('div',{'class' :'question unfound_tips'})
@@ -19,9 +22,18 @@ def found_result(soup):
         return False
 
 
+def output_result(line,b_newline=True):
+    global  _result_file
+    if _DEBUG:
+        print(line)
+    if b_newline:
+        _result_file.write(line+'\n')
+    else:
+        _result_file.write(line)
+
 def fetch_data(word_lst):
 
-    # html_file="./sample.html"
+
     for word in word_lst:
 
         if len(word.split()) > 4 : # stop lookup a sentence
@@ -38,31 +50,32 @@ def fetch_data(word_lst):
         if not found_result(soup):
             continue
 
-        print("\n[%s]" % word )
+        output_result("\n[%s] " % word , False)
 
         soup_translate =soup.find('div', {'class' :'group_pos'})
+
+        # Chinese meaning
         for lbl in soup_translate.findChildren('label'):
-            sys.stdout.write( jtof( lbl.getText().strip()))
-        print ""
+            output_result( jtof( lbl.getText().strip()) , False)
+        output_result('')
         soup_sent = soup.findAll('dl',{'class':'vDef_list'})
-
-
+        # example sentence
         for each_sent in soup_sent:
             res_word="".join(word.split())
             replace_word=" ".join(word.split())
             sent = each_sent.find('dt').getText().replace(res_word," "+replace_word+" ")
-            print sent
-            print jtof(each_sent.find('dd').getText())
-
+            output_result('\t'+sent)
+            output_result('\t'+jtof(each_sent.find('dd').getText()))
 
     pass
 
-
 def main():
-    # print("iciba test")
-    note_lst = parse_notes.read_file(sys.argv[2])
+    global  _result_file
+    note_lst = parse_notes.read_file(sys.argv[1])
 
+    _result_file=open(_RESULT_FILE_NAME,'w')
     fetch_data(note_lst)
+    _result_file.close()
 
 
 
